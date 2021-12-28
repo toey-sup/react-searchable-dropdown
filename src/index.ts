1cbe337ae45743154e2acdb9ec3af5166854e7cf
toey <supanatwongwi@gmail.com>
2021-12-28 10:53:33 +0700

    update import

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from './MultipleSelector/MultipleSelector';
-export * from './Selector/Selector';
+import { MultipleChoice, MultipleChoiceSection, MultipleSelector } from './MultipleSelector/MultipleSelector';
+import { Selector, Choice, ChoiceSection } from './Selector/Selector';
+
+export type { Choice, ChoiceSection, MultipleChoice, MultipleChoiceSection}
+export { MultipleSelector, Selector };
