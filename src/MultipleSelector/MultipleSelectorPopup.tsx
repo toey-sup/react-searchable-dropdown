0be436ed06c5d251e062c3734caf00f1858e6b47
toey <supanatwongwi@gmail.com>
2022-01-11 13:46:22 +0700

    fix bug

--- src/MultipleSelector/MultipleSelectorPopup.tsx
+++ src/MultipleSelector/MultipleSelectorPopup.tsx
@@ -215,7 +215,7 @@ const MultipleSelectorPopup: React.FC<Props> = ({
                     <MultipleSectionItem
                       key={`item-${index + 1}`}
                       choice={choice}
-                      checked={Boolean(chosenChoice[`${choice.singleChoice ? 'Single -' : ''}${choice?.id ?? choice.label}`] || choice.checked)}
+                      checked={Boolean(chosenChoice[`${choice.singleChoice ? 'Single -' : ''}${choice?.id ?? choice.label}`])}
                       handleSelect={handleSelect}
                       className={itemClassName}
                       id={id}
